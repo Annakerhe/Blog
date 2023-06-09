859cdad9da1c0814ef40984d8331e74741273e14
Annakerche <ms.irbis69@gmail.com>
2023-06-09 07:34:12 +0300

    токен авторизация

--- src/main/java/ru/vsu/spring/blogapp/repository/AuthorRepository.java
+++ src/main/java/ru/vsu/spring/blogapp/repository/AuthorRepository.java
@@ -5,17 +5,15 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import ru.vsu.spring.blogapp.domain.entity.AuthorEntity;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface AuthorRepository extends JpaRepository<AuthorEntity, Long> {
     Optional<AuthorEntity> findByFullName(String fullName);
 
+    Optional<AuthorEntity> findByLogin(String login);
+
     @Modifying
     @Query("DELETE FROM AuthorEntity WHERE id = :id")
     void delete(Long id);
 
-//    @Query("SELECT * FROM AuthorEntity WHERE login = :login")
-
-    Optional<AuthorEntity> findByLogin(String login);
 }
